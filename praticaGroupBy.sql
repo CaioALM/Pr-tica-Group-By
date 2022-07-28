eb207e8d0a6d35e2f9184397902c741f8ed5dc0e
Caio <caiosobreira77@gmail.com>
2022-07-28 17:19:23 -0300

    Final version with Bônus

--- praticaGroupBy.sql
+++ praticaGroupBy.sql
@@ -1,11 +1,11 @@
 -- Questão 1)
 
-SELECT COUNT("endDate") as "currentExperiences" 
+SELECT COUNT("endDate") AS "currentExperiences" 
 FROM experiences;
 
 -- Questão 2) 
 
-SELECT u.id, COUNT(e.id) as educations
+SELECT u.id, COUNT(e.id) AS educations
 FROM users u
 JOIN educations e
 ON u.id=e."userId"
@@ -13,7 +13,7 @@ GROUP BY u.id;
 
 -- Questão 3)
 
-SELECT u.name as writer, COUNT(t) as testimonailCount
+SELECT u.name AS writer, COUNT(t) AS testimonailCount
 FROM users u 
 JOIN testimonials t
 ON t."writerId" = u.id
@@ -22,7 +22,7 @@ GROUP BY u.id ;
 
 -- Questão 4)
 
-SELECT MAX(salary) as maximumSalary , r.name as role
+SELECT MAX(salary) AS maximumSalary , r.name AS role
 FROM jobs j
 JOIN roles r
 ON r.id = j."roleId"
@@ -30,4 +30,14 @@ WHERE j.active = true
 GROUP BY r.name
 ORDER BY maximumSalary ASC;
 
+-- Questão Bônus 
 
+SELECT s.name AS school, c.name AS course, COUNT(e) AS "studantsCount" 
+FROM educations e
+JOIN schools s 
+ON s.id = e."schoolId"  
+JOIN courses c 
+ON c.id = e."courseId"
+GROUP BY s.id, c.id
+ORDER BY "studantsCount" DESC
+LIMIT 3;
